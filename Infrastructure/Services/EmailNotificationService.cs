53bc5e29d652b7bf4cb95898c4508112ce9b5ca2
Michal <m.szmeja98@gmail.com>
2023-12-08 14:45:03 +0100

    Prawidłowa implementacja wysyłki powiadomienia email.

--- Infrastructure/Services/EmailNotificationService.cs
+++ Infrastructure/Services/EmailNotificationService.cs
@@ -1,19 +1,32 @@
 ﻿using Domain.Ports;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
 
 namespace Infrastructure.Services
 {
     // Adapter architektury heksagonalnej
     public class EmailNotificationService : INotificationService
     {
+        string smtpHost = "h2.hitme.pl";
+        int smtpPort = 587;
+        string smtpUser = "system@test1234.hmcloud.pl";
+        string smtpPassword = "c4Ev5nIZKXdtJK6CTKe";
+
         public void SendAssignmentNotification(string email, string taskTitle)
         {
-            // Implementacja wysyłania e-maila
-            Console.WriteLine($"Email sent to {email}: Task '{taskTitle}' assigned.");
+            MailMessage mail = new MailMessage();
+            SmtpClient smtp = new SmtpClient(smtpHost);
+
+            mail.From = new MailAddress(smtpUser);
+            mail.To.Add(email);
+            mail.Subject = $"Zadanie {taskTitle} przypisane.";
+            mail.Body = $"Zadanie {taskTitle} przypisane. Zapoznaj się z jego treścią.";
+
+            smtp.Port = smtpPort;
+            smtp.Credentials = new NetworkCredential(smtpUser, smtpPassword);
+            smtp.EnableSsl = true;
+
+            smtp.Send(mail);
         }
     }
 }
